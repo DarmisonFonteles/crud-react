a4dbff99ba8bfc307b83fb2754639d7e4d18868c
Darmison Fonteles <darmisonaugusto@gmail.com>
2023-06-11 19:18:16 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+import Layout from '@/components/Layout'
+import Tabela from '@/components/Tabela'
+import Cliente from '@/core/Cliente'
+import Image from 'next/image'
+
+export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 45, '2'),
+    new Cliente('Carlos', 23, '4'),
+    new Cliente('Pedro', 54, '5'),
+  ]
+
+  return (
+    <div className={`flex h-screen justify-center items-center bg-gradient-to-r from-purple-500 to-blue-600 text-white`}>
+      <Layout titulo="Cadastro Simples">
+        <Tabela clientes={clientes}></Tabela>
+      </Layout>
+    </div>
+  )
+}

c5f47c0e5d66d824617adaff2621e7abb5c1fd8a
Darmison Fonteles <darmisonaugusto@gmail.com>
2023-07-10 21:17:28 -0300

    projeto finalizado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,48 +3,28 @@ import Botao from '@/components/Botao'
 import Formulario from '@/components/Formulario'
 import Layout from '@/components/Layout'
 import Tabela from '@/components/Tabela'
-import Cliente from '@/core/Cliente'
-import Image from 'next/image'
-import { useState } from 'react'
+import useClientes from '@/hooks/useClientes'
 
 export default function Home() {
-  
-  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
-  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela') 
 
-  const clientes = [
-    new Cliente('Ana', 34, '1'),
-    new Cliente('Bia', 45, '2'),
-    new Cliente('Carlos', 23, '4'),
-    new Cliente('Pedro', 54, '5'),
-  ]
+  const { 
+    cliente,
+    clientes,
+    novoCliente,
+    salvarCliente,
+    selecaocliente,
+    Excluircliente,
+    tabelaVisivel,
+    exibirTabela,
 
-  function selecaocliente(cliente: Cliente) {
-    setCliente(cliente)
-    setVisivel('form')
-  }
-
-  function Excluircliente(cliente: Cliente) {
-    console.log(cliente.nome)
-  }
-
-
-  function novoCliente() {
-    setCliente(Cliente.vazio())
-    setVisivel('form')
-  }
-
-  function salvarCliente(cliente: Cliente) {
-    console.log(cliente)
-    setVisivel('tabela')
-  }
+  } = useClientes()
 
 
   return (
     
     <div className={`flex h-screen justify-center items-center bg-gradient-to-r from-purple-500 to-blue-600 text-white`}>
       <Layout titulo="Cadastro Simples">
-        {visivel === 'tabela' ? (
+        {tabelaVisivel ? (
           <>
             <div className='flex justify-end'>
               <Botao cor='green' className='mb-4'
@@ -62,7 +42,7 @@ export default function Home() {
           <Formulario 
             cliente={cliente} 
             clienteMudou={salvarCliente}
-            cancelado={() => setVisivel('tabela')} 
+            cancelado={() => exibirTabela} 
           /> 
         )}
       </Layout>
